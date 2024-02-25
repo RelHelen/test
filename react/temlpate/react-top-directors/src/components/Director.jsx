ce2f39a57f4ad368530b691dc1b200993a63a896
stepRel <itstep.rnd@gmail.com>
2024-02-26 00:33:37 +0300

    add form

--- react/temlpate/react-top-directors/src/components/Director.jsx
+++ react/temlpate/react-top-directors/src/components/Director.jsx
@@ -1,23 +1,38 @@
-
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './Director.css';
 
 let directors =[
   {
+    id:1,
     name:'James',
-    lastname:'Cameron'
+    lastname:'Cameron' 
+
   },
   {
+    id:2,
     name:'Quentin',
     lastname:'Tarantino'
   },
   {
+    id:3,
     name:'George ',
     lastname:'Lucas'
   }
 ]
+// Steven Allan Spielberg
+function Form(){
+  const handleSubmit = event=>{
+    event.preventDefault();
+    console.log('submit отменен');
 
+  }
+  return(
+    <form className="form" onSubmit={ handleSubmit }>
+       <input id="new-dir" name="new-dir" type="text" />
+       <input type="submit" />
+    </form >
+  )
+}
 function Director(props) {
   const {name,lastname} = props;
   return (
@@ -31,10 +46,25 @@ function Director(props) {
 function DirectorList(){
   return(
     <>
-    {/* произйдет  */}
-    <Director {...directors[0]}/> 
+     {/* 1способ */}
+    {/* <Director {...directors[0]}/> 
     <Director {...directors[1]}/> 
-    <Director {...directors[2]}/> 
+    <Director {...directors[2]}/>  */}
+    
+    <div className="directors">
+     {/*2способ */}
+    { 
+    directors.map(director => {
+      return (
+       
+        <Director 
+        { ...director } 
+        key={director.id}
+      />
+      )})
+     }
+     </div>
+     <Form />
     </>
   )
 }
